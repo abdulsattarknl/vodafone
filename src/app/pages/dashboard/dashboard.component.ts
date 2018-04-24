68c74d0c8daea0490467e46b1baae0dd5b1143d3
abdulifthekar <ifthekar.zulf@@gmail.com>
2018-04-24 05:31:42 +0530

    UX screens are ready

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+import { ModalComponent } from '../../core/modal/modal.component';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.html',
+  styleUrls:['./dashboard.css']
+})
+export class DashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
